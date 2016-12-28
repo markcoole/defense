a41b9da4dd6a8e1e1040aa3b260a439f73b7ffd4
Mark Coole <mark.coole@btinternet.com>
2016-12-28 22:58:28 +0000

    Create Shielded Invader class

---
+++ ShieldedInvader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefenseGame
+{
+    class ShieldedInvader : Invader
+    {
+        public ShieldedInvader(Path path) : base(path)
+        {
+
+        }
+
+    }
+}
