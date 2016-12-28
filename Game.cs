1da25337c80c6baf1e2bcc51d851c32cfc2ba214
Mark Coole <mark.coole@btinternet.com>
2016-12-28 23:48:36 +0000

    Add StrongInvader class and change Health accessibility to protected

--- Game.cs
+++ Game.cs
@@ -31,7 +31,7 @@ namespace DefenseGame
                 {
                     new ShieldedInvader(path),
                     new FastInvader(path),
-                    new Invader(path),
+                    new StrongInvader(path),
                     new Invader(path)
                 };
 
--- Invader.cs
+++ Invader.cs
@@ -15,7 +15,7 @@ namespace DefenseGame
 
         public MapLocation Location => _path.GetLocationAt(_pathStep);
 
-        public virtual int Health { get; private set; } = 2;
+        public virtual int Health { get; protected set; } = 2;
 
         public bool HasScored { get { return _pathStep >= _path.Length; } }
 
