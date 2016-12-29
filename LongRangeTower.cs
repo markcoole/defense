a0e6cafeff26527e21b9f0b605769ce71f027137
Mark Coole <mark.coole@btinternet.com>
2016-12-29 17:43:30 +0000

    Add LongRangeTower class

---
+++ LongRangeTower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefenseGame
+{
+    class LongRangeTower : Tower
+    {
+        protected override int Range { get; } = 4;
+ 
+        protected override string TowerType { get; } = "Long Range Tower";
+
+        public LongRangeTower(MapLocation location) : base(location)
+        { }
+    }
+}
